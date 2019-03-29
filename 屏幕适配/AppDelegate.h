8528489a950224e029e116d904711731fe291107
1174062992 <1174062992>
2019-03-30 02:07:34 +0800

    Initial Commit

---
+++ 屏幕适配/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  屏幕适配
+//
+//  Created by 曹记 on 2019/3/30.
+//  Copyright © 2019 曹记. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 屏幕适配/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  屏幕适配
+//
+//  Created by 曹记 on 2019/3/30.
+//  Copyright © 2019 曹记. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
